f7b992a8b8d1438a3253d75b64d85634f7e5dc30
Renee Messersmith <morganrmessersmith@gmail.com>
2025-03-03 16:16:35 -0500

    Feat: added cat gif and removed totoro

--- src/renderer/welcome/welcome.tsx
+++ src/renderer/welcome/welcome.tsx
@@ -2,18 +2,18 @@ import './welcome.scss';
 import { Link } from 'react-router-dom';
 
 function Welcome() {
-  const totoroGif = require("../assets/totoro.gif");
+  const catGif = require("../assets/cat.gif");
 
 
   return (
     <div className="welcome-page">
       <div className="greeting">
         <h1>Welcome!</h1>
-        <img className="totoro" src={totoroGif}/>
       </div>
       <Link to={'/affirmations'}>
         <button className="home-button" type="button">Click Me</button>
       </Link>
+        <img className="cat" src={catGif}/>
     </div>
   )
 };
